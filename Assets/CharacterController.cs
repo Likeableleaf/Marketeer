e78629ab9165021c3806b312ddc1703ddb0df9a5
Likeableleaf <120080240+Likeableleaf@users.noreply.github.com>
2023-09-26 13:21:21 -0400

    update positions

---
+++ Assets/CharacterController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float speed = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 currentPosition = this.gameObject.transform.position;
+        
+    }
+}
