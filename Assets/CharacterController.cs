9f073715f1e491b0106bca0c930bb1836a39e92d
Likeableleaf <120080240+Likeableleaf@users.noreply.github.com>
2023-09-26 14:06:00 -0400

    update character controlls

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -2,18 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour
+public class CharacterController : GridObject
 {
+    private float speed = 3f;
+   
+
+
     // Start is called before the first frame update
     void Start()
     {
-        float speed = 5f;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 currentPosition = this.gameObject.transform.position;
-        
+        Vector3 newPosition;
+        Vector3 currentPosition = transform.position;
+        newPosition = currentPosition;
+        if (Input.GetKey(KeyCode.W))
+        {
+            newPosition.z +=  speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            newPosition.x +=  speed * Time.deltaTime; 
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            newPosition.x += -speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            newPosition.z += -speed * Time.deltaTime;
+        }
+
+        Vector3 direction = newPosition - currentPosition;
+        if (direction != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 0.3f);
+        }
+        transform.position = newPosition;
+
+
     }
 }
