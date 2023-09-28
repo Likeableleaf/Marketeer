819439f077737819453e3848118c0c5da5cedd65
jkoehle5 <112022021+jkoehle5@users.noreply.github.com>
2023-09-27 23:27:42 -0400

    ShelfFunctions
    Added shelf functionality and animation system for the Milk fridge

--- Assets/Scripts/Shelf.cs
+++ Assets/Scripts/Shelf.cs
@@ -9,16 +9,29 @@ public class Shelf : Wall
 {
     public GameObject[] ShelfVariants;
     public GameObject CurrentShelfObject;
+    public int capacity = 0;
 
     // Start is called before the first frame update
-    protected new void Start()
-    {
+    protected new void Start()    {
         base.Start();
     }
 
     // Update is called once per frame
-    protected new void Update()
-    {
+    protected new void Update()    {
         base.Update();
+    }
+
+    public void FillShelf() {
+        ShelfVariants[capacity].SetActive(false);
+        capacity = 0;
+        ShelfVariants[capacity].SetActive(true);
+    }
+
+    public void ConsumeItems() {
+        if (capacity < 4) {
+            ShelfVariants[capacity].SetActive(false);
+            capacity += 1;
+            ShelfVariants[capacity].SetActive(true);
+        }        
     }
 }
