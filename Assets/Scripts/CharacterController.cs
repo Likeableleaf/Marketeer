6c75b39ea61ee0b21c1b9b7dc30eed1206a2fe7c
Likeableleaf <120080240+Likeableleaf@users.noreply.github.com>
2023-10-12 16:27:43 -0400

    Update CharacterControl for shelf interaction

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class CharacterController : GridObject
 {
     private float speed = 3f;
-   
-
+    private float oldDistance = 9999;
+    private GameObject closetsObject;
 
     // Start is called before the first frame update
     new void Start()
@@ -45,6 +45,40 @@ public class CharacterController : GridObject
         }
         transform.position = newPosition;
 
+        //TODO: add an inventory for each item type for the player
 
+        //TODO: Add a way to detect next to a shelf
+        if (Input.GetKey(KeyCode.Space))
+        {GameObject ObjectClosest = GetNearestObject(5);
+            if(ObjectClosest.layer == LayerMask.NameToLayer("Shelf"))
+            {
+                Shelf currentShelf = (Shelf)ObjectClosest.GetComponent(typeof(Shelf));
+                if (currentShelf.name == "BeanShelf"){
+                    //TODO: Change names of all shelves to be JUST their names of their respective items and add here more shelf names to check...
+                    currentShelf.RefillShelf(0);
+                }
+            }
+        }
+    }
+
+    //grab the closes object
+    private GameObject GetNearestObject(float distance)
+    {
+        List<GameObject> NearGameobjects = new List<GameObject>();
+        if (distance != 0)
+        {
+            oldDistance = distance;
+        }
+        foreach (GameObject g in NearGameobjects)
+        {
+            float dist = Vector3.Distance(this.gameObject.transform.position, g.transform.position);
+            if (dist < oldDistance)
+            {
+                closetsObject = g;
+                oldDistance = dist;
+            }
+        }
+        
+        return closetsObject;
     }
 }
