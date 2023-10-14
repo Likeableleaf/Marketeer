50df36e4f0b002aad131af93e02edc94f3c97644
Likeableleaf <120080240+Likeableleaf@users.noreply.github.com>
2023-10-14 13:02:25 -0400

    updated player inventory

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -6,12 +6,20 @@ public class CharacterController : GridObject
 {
     private float speed = 3f;
     private float oldDistance = 9999;
-    private GameObject closetsObject;
+    private GameObject closetsObject = null;
+    
+    ArrayList Inventory = new ArrayList();
 
     // Start is called before the first frame update
     new void Start()
     {
-
+        //TODO: add an inventory for each item type for the player
+        Inventory[0] = 8; // [0]number of Milk
+        Inventory[1] = 8; // [1]number of Eggs
+        Inventory[2] = 8; // [2]number of Bread
+        Inventory[3] = 8; // [3]number of Beans
+        Inventory[4] = 8; // [4]number of Meat
+        Inventory[5] = 8; // [5]number of Vegatables
     }
 
     // Update is called once per frame
@@ -45,15 +53,45 @@ public class CharacterController : GridObject
         }
         transform.position = newPosition;
 
-        //TODO: add an inventory for each item type for the player
+        
 
-        //TODO: Add a way to detect next to a shelf
+        //TODO: add a way to restock inventory for the player
+        //TODO: track a way to add all the inventory slots so it does not go over 48
+
+        //TODO: Add a way to detect next to a shelf and allow the 0 for each refillshelf(); method to be the inventory number of that item the player has
         if (Input.GetKey(KeyCode.Space))
         {GameObject ObjectClosest = GetNearestObject(5);
-            if(ObjectClosest.layer == LayerMask.NameToLayer("Shelf"))
-            {
-                Shelf currentShelf = (Shelf)ObjectClosest.GetComponent(typeof(Shelf));
+            //check if object detected is a shelf
+            if( ( ObjectClosest != null ) && ( ObjectClosest.layer == LayerMask.NameToLayer( "Shelf" ) ) )
+            {   //instantiate object to be shelf object to use it's methods
+                Shelf currentShelf = ( Shelf )ObjectClosest.GetComponent( typeof( Shelf ) );
+                //check if its a bean shelf
                 if (currentShelf.name == "BeanShelf"){
+                    //TODO: Change names of all shelves to be JUST their names of their respective items and add here more shelf names to check...
+                    currentShelf.RefillShelf(0);
+                }
+                if (currentShelf.name == "MeatShelf")
+                {
+                    //TODO: Change names of all shelves to be JUST their names of their respective items and add here more shelf names to check...
+                    currentShelf.RefillShelf(0);
+                }
+                if (currentShelf.name == "EggsShelf")
+                {
+                    //TODO: Change names of all shelves to be JUST their names of their respective items and add here more shelf names to check...
+                    currentShelf.RefillShelf(0);
+                }
+                if (currentShelf.name == "VeggiesShelf")
+                {
+                    //TODO: Change names of all shelves to be JUST their names of their respective items and add here more shelf names to check...
+                    currentShelf.RefillShelf(0);
+                }
+                if (currentShelf.name == "BreadShelf")
+                {
+                    //TODO: Change names of all shelves to be JUST their names of their respective items and add here more shelf names to check...
+                    currentShelf.RefillShelf(0);
+                }
+                if (currentShelf.name == "MilkShelf")
+                {
                     //TODO: Change names of all shelves to be JUST their names of their respective items and add here more shelf names to check...
                     currentShelf.RefillShelf(0);
                 }
@@ -78,7 +116,15 @@ public class CharacterController : GridObject
                 oldDistance = dist;
             }
         }
+        //Check if there was a objected that was a certain distance
+        if(closetsObject != null)
+        {
+            return closetsObject;
+        }
+        else
+        {
+            return null;
+        }
         
-        return closetsObject;
     }
 }
