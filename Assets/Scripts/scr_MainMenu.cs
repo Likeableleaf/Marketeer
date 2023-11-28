4d9f3e9a027eb38b8fffd24ae370375d240a0452
jkoehle5 <112022021+jkoehle5@users.noreply.github.com>
2023-11-28 10:21:20 -0500

    Shennanagins

--- Assets/Scripts/scr_MainMenu.cs
+++ Assets/Scripts/scr_MainMenu.cs
@@ -5,10 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class scr_MainMenu : MonoBehaviour
 {
+    // Variable Cache
+    [SerializeField] private Camera menuCam;
+    [SerializeField] private Camera inGameCam;
+    [SerializeField] private GameObject inGameUI;
+    [SerializeField] private GameObject menuUI;
+    [SerializeField] private GameObject ceiling;
+
+    private void Start() {
+        inGameCam = Camera.main;
+        menuCam.enabled = true;
+        inGameCam.enabled = false;
+        ceiling.SetActive(true);
+        
+    }
+
     // Starts game
     public void Play()
     {
-        SceneManager.LoadScene("Store");
+        // change main camera + ui to in game, versions
+        
+        inGameCam.enabled = true;
+        menuUI.SetActive(false);
+        inGameUI.SetActive(true);
+        ceiling.SetActive(false);
+        menuCam.enabled = false;
+        //*/
+        //SceneManager.LoadScene("Store");
     }
 
     // Exits game
