82fa39f295da2cdfc8874b629009a93d4f0746fa
jkoehle5 <112022021+jkoehle5@users.noreply.github.com>
2023-11-28 09:28:13 -0800

    Menu Condensed
    * Shennanagins
    
    * Fixed mostly

--- Assets/Scripts/GameUIManager.cs
+++ Assets/Scripts/GameUIManager.cs
@@ -7,11 +7,26 @@ public class GameUIManager : MonoBehaviour {
     // Variables
     [SerializeField] private static bool paused = false;
     [SerializeField] private GameObject obj_pauseMenu;
+    [SerializeField] private GameObject cam;
     [SerializeField] private InGameUI obj_UI;
+    [SerializeField] private Transform inGameCam;
+    [SerializeField] private Transform menuGameCam;
+    [SerializeField] private GameObject inGameUI;
+    [SerializeField] private GameObject menuUI;
+    [SerializeField] private GameObject ceiling;
+    public bool runnin;
 
+    private void Start() {
+        runnin = false;
+        Time.timeScale = 0f;
+        cam.transform.position = menuGameCam.position;
+        cam.transform.rotation = menuGameCam.rotation;
+        ceiling.SetActive(true);
+    }
+    
     // If esc is pressed pause game or resume based on if it is already or not
     void Update() {
-        if (Input.GetKeyDown(KeyCode.Escape)) {
+        if (Input.GetKeyDown(KeyCode.Escape) && runnin) {
             if (paused) {
                 Resume();
             } else {
@@ -44,5 +59,26 @@ public class GameUIManager : MonoBehaviour {
     public void QuitGame () {
         Time.timeScale = 1f;
         SceneManager.LoadScene("MainMenu");
+    }
+
+    // Starts game
+    public void Play()
+    {
+        // change main camera + ui to in game, versions
+        runnin = true;
+        Time.timeScale = 1f;
+        cam.transform.position = inGameCam.position;
+        cam.transform.rotation = inGameCam.rotation;
+        menuUI.SetActive(false);
+        inGameUI.SetActive(true);
+        ceiling.SetActive(false);
+        //*/
+        //SceneManager.LoadScene("Store");
+    }
+
+    // Exits game
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
--- Assets/Scripts/scr_MainMenu.cs
+++ Assets/Scripts/scr_MainMenu.cs
@@ -5,10 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class scr_MainMenu : MonoBehaviour
 {
+    // Variable Cache
+    [SerializeField] private GameObject cam;
+    [SerializeField] private Transform inGameCam;
+    [SerializeField] private Transform menuGameCam;
+    [SerializeField] private GameObject inGameUI;
+    [SerializeField] private GameObject menuUI;
+    [SerializeField] private GameObject ceiling;
+
+    private void Start() {
+        cam.transform.position = menuGameCam.position;
+        cam.transform.rotation = menuGameCam.rotation;
+        ceiling.SetActive(true);
+    }
+
     // Starts game
     public void Play()
     {
-        SceneManager.LoadScene("Store");
+        // change main camera + ui to in game, versions
+        
+        cam.transform.position = inGameCam.position;
+        cam.transform.rotation = inGameCam.rotation;
+        menuUI.SetActive(false);
+        inGameUI.SetActive(true);
+        ceiling.SetActive(false);
+        //*/
+        //SceneManager.LoadScene("Store");
     }
 
     // Exits game
